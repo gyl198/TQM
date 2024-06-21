f7007496f1776b1b728331ebefd3f902ea6787a3
gyl198 <2210433038@email.szu.edu.cn>
2024-06-21 22:49:54 +0800

    init

---
+++ n_tqm/n_tqm/CMakeFiles/n_tqm-tofino.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/n_tqm-tofino"
+  "n_tqm/tofino/bf-rt.json"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/n_tqm-tofino.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ p_tqm/p_tqm/CMakeFiles/p_tqm-tofino.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p_tqm-tofino"
+  "p_tqm/tofino/bf-rt.json"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/p_tqm-tofino.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
